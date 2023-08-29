125bee0221cc5d9f999498a265501bc9e0fcf51d
Varun <varunjethani@outlook.com>
2023-08-29 22:21:07 +0530

    added equal functionality and percent functionality and updated the power of functionality

--- app/src/main/java/com/example/calulator_mycap/MainActivity.java
+++ app/src/main/java/com/example/calulator_mycap/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class MainActivity extends AppCompatActivity {
     TextView workingsTV;
@@ -30,14 +35,67 @@ public class MainActivity extends AppCompatActivity {
         workings = workings + givenValue;
         workingsTV.setText(workings);
     }
+
+    public void equalsOnClick(View view){
+        Double result = null;
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        formula=workings;
+        checkForPowerOf();
+        checkForPercent();
+        try{
+            result = (double)engine.eval(formula);
+        }
+        catch (ScriptException e){
+            Toast.makeText(this,"Invalid Input", Toast.LENGTH_SHORT).show();
+        }
+        if (result != null) {
+            resultsTV.setText(String.valueOf(result.doubleValue()));
+        }
+
+    }
+
+    private void checkForPercent(){
+        ArrayList<Integer> indexOfPercents = new ArrayList<>();
+        for (int i=0; i<workings.length();i++){
+            if (workings.charAt(i) == '%'){
+                indexOfPercents.add(i);
+            }
+            tempFormula = formula;
+            for (Integer indexP : indexOfPercents){
+                changeFormulaPer(indexP);
+            }
+            formula = tempFormula;
+        }
+    }
+
+    private void changeFormulaPer(Integer indexP) {
+        String numberLeftP = "";
+        String numberRightP = "";
+
+        for (int i = indexP + 1; i < workings.length(); i++) {
+            if (isNumeric(workings.charAt(i))) {
+                numberRightP = numberRightP + workings.charAt(i);
+            } else
+                break;
+        }
+
+        for (int i = indexP - 1; i >= 0; i--) {
+            if (isNumeric(workings.charAt(i))) {
+                numberLeftP = workings.charAt(i) + numberLeftP;
+            } else
+                break;
+        }
+        String original = numberLeftP + "%" + numberRightP;
+        String changed = "(" + numberLeftP + "/100)*" + numberRightP;
+        tempFormula = tempFormula.replace(original, changed);
+    }
     private void checkForPowerOf(){
         ArrayList<Integer> indexOfPowers = new ArrayList<>();
         for (int i=0; i<workings.length();i++){
             if (workings.charAt(i) == '^'){
                 indexOfPowers.add(i);
             }
-            formula = workings;
-            tempFormula = workings;
+            tempFormula = formula;
             for (Integer index : indexOfPowers){
                 changeFormula(index);
             }
@@ -58,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        for (int i= index-1; i<=0;i--){
+        for (int i= index-1; i>=0;i--){
             if (isNumeric(workings.charAt(i))){
                 numberLeft = workings.charAt(i) + numberLeft;
             }
@@ -66,7 +124,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         String original = numberLeft+"^"+numberRight;
-        String changed = "Math.pow("+numberLeft+","+numberRight+")";
+        String changed = "(Math.pow("+numberLeft+","+numberRight+"))";
         tempFormula = tempFormula.replace(original, changed);
     }
 
@@ -98,6 +156,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void powerOfOnClick(View view){
         setWorkings("^");
+    }
+    public void percentOnClick(View view){
+        setWorkings("%");
     }
     public void addOnClick(View view){
         setWorkings("+");
@@ -144,12 +205,10 @@ public class MainActivity extends AppCompatActivity {
     public void nineOnClick(View view){
         setWorkings("9");
     }
-    public void inverseOnClick(View view){
-        setWorkings("-");
-    }
-//TODO create the final equals on click
-//TODO then create better functionality by creating +/- and % functions
-//TODO create better functionality for ()
+
+
+
+
 //TODO create more operations
 
 }
